9282a56a1e3a190ab29d00cf4e7152b154c709f0
Allanxd0621 <allandinomanalo@gmail.com>
2026-04-30 20:53:50 +0800

    Update click.js

--- click.js
+++ click.js
@@ -2,8 +2,23 @@ let click = 0;
 
 document.getElementById('click').onclick = function(){
     click++;
-    if(click == 10){
-        alert('you spanked me 10 times daddy');
+    if(click == 5){
+        alert('faster daddy');
+    }
+    else if(click == 10){
+        alert('damnn ughh..');
+    }
+    else if(click == 15){
+        alert('ugh faster daddy..');
+    }
+    else if(click == 20){
+        alert('im almost there..');
+    }
+    else if(click == 30){
+        alert('ugh Im cominggg!!!');
+    }
+    else if(click == 40){
+        alert('💦💦💦💦');
     }
 }// this is for the clicks
 
@@ -11,3 +26,8 @@ document.getElementById('show').onclick = function(){
     alert('The number of Clicks: ' + click);
     
 }// this is to show the number of clicks made by the click++ 
+
+document.getElementById('reset').onclick = function(){
+    alert('Clicks back to 0');
+    click = 0 ;
+}
