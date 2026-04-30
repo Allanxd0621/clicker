05c9ffffd8e81c59fb0d4fcecb4abdb8541aa446
Allanxd0621 <allandinomanalo@gmail.com>
2026-04-30 19:52:11 +0800

    Update click.js

--- click.js
+++ click.js
@@ -2,11 +2,12 @@ let click = 0;
 
 document.getElementById('click').onclick = function(){
     click++;
+    if(click == 10){
+        alert('you spanked me 10 times daddy');
+    }
 }// this is for the clicks
 
 document.getElementById('show').onclick = function(){
     alert('The number of Clicks: ' + click);
-    if(click = 10){
-        alert('you spank me 10 times daddy');
-    }
+    
 }// this is to show the number of clicks made by the click++ 
